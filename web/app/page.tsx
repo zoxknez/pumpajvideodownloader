5158cb386e19f2859f384312bb3abd6fd2b4f32b
zoxknez <zoxknez@hotmail.com>
2025-10-05 18:08:27 +0200

    Use DownloaderHome component for main app

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,14 +2,14 @@
 'use client';
 
 import { AuthProvider, LoginGate } from '@/components/AuthProvider';
-import DesktopApp from '@/components/DesktopApp';
+import DownloaderHome from '@/components/DownloaderHome';
 
 export default function HomePage() {
   return (
     <AuthProvider>
       <LoginGate>
-        {/* When user is logged in, show the main application */}
-        <DesktopApp />
+        {/* When user is logged in, show the main downloader application */}
+        <DownloaderHome />
       </LoginGate>
     </AuthProvider>
   );
