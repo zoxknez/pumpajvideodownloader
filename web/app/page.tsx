1918400fc32224b42478a3b9c764e73f2cfda1d5
zoxknez <zoxknez@hotmail.com>
2025-10-05 18:10:58 +0200

    Use AppShell component for full app UI with header and sidebar

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,14 +2,14 @@
 'use client';
 
 import { AuthProvider, LoginGate } from '@/components/AuthProvider';
-import DownloaderHome from '@/components/DownloaderHome';
+import { AppShell } from '@/components/AppShell';
 
 export default function HomePage() {
   return (
     <AuthProvider>
       <LoginGate>
-        {/* When user is logged in, show the main downloader application */}
-        <DownloaderHome />
+        {/* When user is logged in, show the full app with header, sidebar, tabs, etc */}
+        <AppShell />
       </LoginGate>
     </AuthProvider>
   );
